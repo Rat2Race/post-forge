dc36cfc295c6129b75ad8c354adecd848e05103a
M1NS1K <rlaalstlr2001@gmail.com>
2024-08-09 06:28:23 +0900

    [feat] ArticleRepo create

---
+++ src/main/java/com/springweb/study/repository/ArticleRepo.java
@@ -0,0 +1,9 @@
+package com.springweb.study.repository;
+
+import com.springweb.study.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepo extends JpaRepository<Article, Long> {
+}
