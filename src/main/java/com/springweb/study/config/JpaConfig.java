f719ecce6d38d48f370c59f5b6940081c7558bc2
M1NS1K <rlaalstlr2001@gmail.com>
2024-08-09 02:06:14 +0900

    [feat] JpaConfig setting

---
+++ src/main/java/com/springweb/study/config/JpaConfig.java
@@ -0,0 +1,24 @@
+package com.springweb.study.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+	@Bean
+	public AuditorAware<String> auditorAware() {
+		return () -> Optional.of(
+				SecurityContextHolder
+						.getContext()
+						.getAuthentication()
+						.getName()
+		);
+	}
+}
