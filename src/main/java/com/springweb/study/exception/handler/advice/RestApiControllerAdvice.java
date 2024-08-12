1eaeef9f257ddfdfa17e95d9d48787a11de088ea
M1NS1K <rlaalstlr2001@gmail.com>
2024-08-13 00:19:38 +0900

    [feat] rest api controller advice add badRequest exception

---
+++ src/main/java/com/springweb/study/exception/handler/advice/RestApiControllerAdvice.java
@@ -0,0 +1,21 @@
+package com.springweb.study.exception.handler.advice;
+
+import com.springweb.study.exception.BadRequestException;
+import com.springweb.study.exception.handler.ErrorResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice(basePackages = "messageboard.controller")
+public class RestApiControllerAdvice {
+
+	@ExceptionHandler
+	public ResponseEntity<ErrorResult> badRequest(BadRequestException e) {
+		log.error("[401 에러]", e);
+		ErrorResult errorResult = new ErrorResult("BadRequest-EX", e.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResult);
+	}
+}
