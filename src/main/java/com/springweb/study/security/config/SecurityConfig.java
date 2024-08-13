baf1629f9b7cd6219d31bbe35a5ac6ed572dd9b1
M1NS1K <rlaalstlr2001@gmail.com>
2024-08-14 03:49:42 +0900

    [feat] security config add filter beans

--- src/main/java/com/springweb/study/security/config/SecurityConfig.java
+++ src/main/java/com/springweb/study/security/config/SecurityConfig.java
@@ -1,8 +1,14 @@
 package com.springweb.study.security.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springweb.study.security.filter.JsonUsernamePasswordAuthenticationFilter;
+import com.springweb.study.security.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,6 +21,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final UserDetailsServiceImpl userDetailsService;
+	private final ObjectMapper objectMapper;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -36,7 +45,31 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public static PasswordEncoder bCryptPasswordEncoder() {
+	public DaoAuthenticationProvider daoAuthenticationProvider() throws Exception {
+		DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+
+		daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+		daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+
+		return daoAuthenticationProvider;
+	}
+
+	@Bean
+	public static PasswordEncoder passwordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager() throws Exception {
+		DaoAuthenticationProvider provider = daoAuthenticationProvider();
+		provider.setPasswordEncoder(passwordEncoder());
+		return new ProviderManager(provider);
+	}
+
+	@Bean
+	public JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordAuthenticationFilter() throws Exception {
+		JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordAuthenticationFilter = new JsonUsernamePasswordAuthenticationFilter(objectMapper);
+		jsonUsernamePasswordAuthenticationFilter.setAuthenticationManager(authenticationManager());
+		return jsonUsernamePasswordAuthenticationFilter;
 	}
 }
