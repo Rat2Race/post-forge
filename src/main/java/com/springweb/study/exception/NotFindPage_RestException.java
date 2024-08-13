40cb8560017334ad216f7e46c1504061f5e11764
M1NS1K <rlaalstlr2001@gmail.com>
2024-08-13 17:37:43 +0900

    [feat] NotFindPage_RestException

---
+++ src/main/java/com/springweb/study/exception/NotFindPage_RestException.java
@@ -0,0 +1,20 @@
+package com.springweb.study.exception;
+
+public class NotFindPage_RestException extends RuntimeException {
+
+    public NotFindPage_RestException() {
+        super();
+    }
+
+    public NotFindPage_RestException(String message) {
+        super(message);
+    }
+
+    public NotFindPage_RestException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotFindPage_RestException(Throwable cause) {
+        super(cause);
+    }
+}
