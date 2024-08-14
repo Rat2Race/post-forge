a415b14792bcc8fbd7406bcd5415b994491a7f13
M1NS1K <rlaalstlr2001@gmail.com>
2024-08-15 01:09:36 +0900

    [feat] UserService

--- src/main/java/com/springweb/study/security/service/UserService.java
+++ src/main/java/com/springweb/study/security/service/UserService.java
@@ -1,4 +1,23 @@
 package com.springweb.study.security.service;
 
+import com.springweb.study.security.domain.User;
+import com.springweb.study.security.domain.dto.AddUserRequest;
+import com.springweb.study.security.repository.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class UserService {
+
+	private final UserRepo userRepo;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	public Long save(AddUserRequest request) {
+		return userRepo.save(User.builder()
+				.email(request.getEmail())
+				.password(bCryptPasswordEncoder.encode(request.getPassword()))
+				.build()).getId();
+	}
 }
