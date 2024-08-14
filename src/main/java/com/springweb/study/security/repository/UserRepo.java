7a10db0ff9193bf9865cb022aab11be3920a79cf
M1NS1K <rlaalstlr2001@gmail.com>
2024-08-14 16:33:49 +0900

    [feat] JwtService interface create and Jwt properties setting

--- src/main/java/com/springweb/study/security/repository/UserRepo.java
+++ src/main/java/com/springweb/study/security/repository/UserRepo.java
@@ -7,4 +7,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
 	User findByEmail(String email);
+	boolean existsByEmail(String email);
+	User findByNumber(String number);
+	boolean existsByNumber(String number);
+	User findByRefreshToken(String refreshToken);
 }
