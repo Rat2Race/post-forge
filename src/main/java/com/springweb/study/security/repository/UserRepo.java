f547997974c67179a50306e5691d6efd07caa601
M1NS1K <rlaalstlr2001@gmail.com>
2024-08-25 17:03:44 +0900

    feat: Admin service set

--- src/main/java/com/springweb/study/security/repository/UserRepo.java
+++ src/main/java/com/springweb/study/security/repository/UserRepo.java
@@ -1,6 +1,8 @@
 package com.springweb.study.security.repository;
 
+import com.springweb.study.common.RoleType;
 import com.springweb.study.domain.User;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -13,4 +15,5 @@ public interface UserRepo extends JpaRepository<User, Long> {
 	Optional<User> findById(UUID id);
 	boolean existsById(UUID id);
 	void deleteById(UUID id);
+	List<User> findAllByType(RoleType type);
 }
