dc10375b256090ae64a86d51aa2d11e873abff5b
M1NS1K <rlaalstlr2001@gmail.com>
2024-08-25 16:47:22 +0900

    feat: admin authorize annotation set

--- src/main/java/com/springweb/study/security/config/SecurityConfig.java
+++ src/main/java/com/springweb/study/security/config/SecurityConfig.java
@@ -1,24 +1,16 @@
 package com.springweb.study.security.config;
 
-import com.springweb.study.security.filter.JwtAuthFilter;
 import com.springweb.study.security.filter.JwtAuthenticationFilter;
-import com.springweb.study.security.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
@@ -37,14 +29,12 @@ public class SecurityConfig {
 				.authorizeHttpRequests(auth -> auth
 						.requestMatchers("/auth/login", "/auth/register").permitAll()
 						.requestMatchers("/css/**", "/js/**", "/images/**", "/webjars/**").permitAll()
-//						.requestMatchers("/auth/dashboard").hasRole("USER")
 						.anyRequest().authenticated() // Protect all other endpoints
 				)
 				.sessionManagement(session -> session
 						.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 				)
-				.authenticationProvider(authenticationProvider())
-				.addFilterBefore(jwtAuthenticationFilter, BasicAuthenticationFilter.class)
+				.addFilterBefore(jwtAuthenticationFilter, BasicAuthenticationFilter.class);
 		return http.build();
 	}
 
@@ -52,16 +42,5 @@ public class SecurityConfig {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	@Bean
-	public AuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-		authenticationProvider.setUserDetailsService(userDetailsService);
-		authenticationProvider.setPasswordEncoder(passwordEncoder());
-		return authenticationProvider;
-	}
 
-	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-		return config.getAuthenticationManager();
-	}
 }
