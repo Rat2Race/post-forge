0af7f52d90e2cee9cd5c0750e5b0980ac90fe1d8
rat <rlaalstlr2001@gmail.com>
2026-01-09 17:05:24 +0900

    chore: 서버용 cors 수정

--- auth/src/main/java/dev/iamrat/security/config/CorsConfig.java
+++ auth/src/main/java/dev/iamrat/security/config/CorsConfig.java
@@ -13,29 +13,29 @@ import java.util.List;
 @Slf4j
 @Configuration
 public class CorsConfig {
-//    @Value("${spring.cors.allowed-origins:${cors.allowed-origins}}")
-//    private List<String> allowedOrigins;
+    @Value("${spring.cors.allowed-origins:${cors.allowed-origins}}")
+    private List<String> allowedOrigins;
     
-    @Value("${cors.allowed-origins}")
-    private String allowedOrigins;
+//    @Value("${cors.allowed-origins}")
+//    private String allowedOrigins;
     
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         
-        //local
-        List<String> localOrigins = List.of(allowedOrigins.split(","));
+//        //local
+//        List<String> localOrigins = List.of(allowedOrigins.split(","));
         
-//        log.info("=== CORS 설정 ===");
-//        log.info("허용된 Origins: {}", allowedOrigins);
-//        log.info("Origins 개수: {}", allowedOrigins.size());
-  
         log.info("=== CORS 설정 ===");
-        log.info("허용된 Origins: {}", localOrigins);
-        log.info("Origins 개수: {}", localOrigins.size());
+        log.info("허용된 Origins: {}", allowedOrigins);
+        log.info("Origins 개수: {}", allowedOrigins.size());
+  
+//        log.info("=== CORS 설정 ===");
+//        log.info("허용된 Origins: {}", localOrigins);
+//        log.info("Origins 개수: {}", localOrigins.size());
         
-//        configuration.setAllowedOrigins(allowedOrigins);
-        configuration.setAllowedOrigins(localOrigins);
+        configuration.setAllowedOrigins(allowedOrigins);
+//        configuration.setAllowedOrigins(localOrigins);
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
